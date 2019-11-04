f29d6e7f6852c73352e75fd7c4b208b23d27a0c0
hyesung <comet_0419@naver.com>
2019-11-04 20:51:11 +0900

    Calendar

--- app/src/main/java/com/example/dalyeodalyeok/MainActivity.java
+++ app/src/main/java/com/example/dalyeodalyeok/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.dalyeodalyeok;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarView calendar = (CalendarView)findViewById(R.id.calendarView);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+                Toast.makeText(MainActivity.this, ""+year+"/"+(month+1)+"/"+dayOfMonth, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
