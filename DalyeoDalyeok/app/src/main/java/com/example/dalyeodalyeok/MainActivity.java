df484fe53595725ff01485e366d479d9538039cd
hyesung <comet_0419@naver.com>
2019-11-13 15:21:45 +0900

    add floatingActiongButton and ScheduleActivity

--- DalyeoDalyeok/app/src/main/java/com/example/dalyeodalyeok/MainActivity.java
+++ DalyeoDalyeok/app/src/main/java/com/example/dalyeodalyeok/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.dalyeodalyeok;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -14,24 +16,38 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private AppBarConfiguration mAppBarConfiguration;
+
+    private Context mContext;
+    private  FloatingActionButton fab;
+    private FloatingActionButton fab_sub1, fab_sub2;
+
+    private Animation fab_open, fab_close;
+
+    private boolean isFabOpen = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        mContext = getApplicationContext();
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab_open = AnimationUtils.loadAnimation(mContext, R.anim.fab_open);
+
+        fab_close = AnimationUtils.loadAnimation(mContext, R.anim.fab_close);
+        fab = findViewById(R.id.fab);
+        fab_sub1 = (FloatingActionButton) findViewById(R.id.fab_sub1);
+
+        fab_sub2 = (FloatingActionButton) findViewById(R.id.fab_sub2);
+        fab.setOnClickListener(this);
+        fab_sub1.setOnClickListener(this);
+        fab_sub2.setOnClickListener(this);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -42,6 +58,74 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.fab:
+
+                toggleFab();
+
+                break;
+
+            case R.id.fab_sub1:
+
+                toggleFab();
+
+                Toast.makeText(this, "Camera Open-!", Toast.LENGTH_SHORT).show();
+
+                break;
+
+
+
+            case R.id.fab_sub2: // 일정추가 버튼
+
+                toggleFab();
+
+                Toast.makeText(this, "Map Open-!", Toast.LENGTH_SHORT).show();
+                Intent intentSchedule = new Intent(MainActivity.this, ScheduleActivity.class);
+                startActivity(intentSchedule);
+
+                break;
+
+        }
+
+    }
+
+    private void toggleFab() {
+
+        if (isFabOpen) {
+
+            fab_sub1.startAnimation(fab_close);
+
+            fab_sub2.startAnimation(fab_close);
+
+            fab_sub1.setClickable(false);
+
+            fab_sub2.setClickable(false);
+
+            isFabOpen = false;
+
+        } else {
+
+//            fab.setImageResource(R.drawable.ic_close);
+
+            fab_sub1.startAnimation(fab_open);
+
+            fab_sub2.startAnimation(fab_open);
+
+            fab_sub1.setClickable(true);
+
+            fab_sub2.setClickable(true);
+
+            isFabOpen = true;
+
+        }
+
     }
 
     @Override
