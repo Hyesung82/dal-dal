f161161fa775d5567886881bb23d7e6b3ffc1700
unknown <soominnm@naver.com>
2019-11-19 21:01:09 +0900

    버튼만듦

--- DalyeoDalyeok/app/src/main/java/com/example/dalyeodalyeok/ScheduleActivity.java
+++ DalyeoDalyeok/app/src/main/java/com/example/dalyeodalyeok/ScheduleActivity.java
@@ -3,6 +3,11 @@ package com.example.dalyeodalyeok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ScheduleActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class ScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        Button OK = (Button)findViewById(R.id.OK);
+        OK.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(view.getId() == R.id.OK) {
+                    btn_Click(view);
+                }
+            }
+        });
+    }
+
+
+    public void btn_Click(View view)
+    {
+        TextView textView = (TextView)findViewById(R.id.textView2);
+        EditText editText = (EditText)findViewById(R.id.schedule);
+
+        textView.setText(editText.getText());
     }
 }
