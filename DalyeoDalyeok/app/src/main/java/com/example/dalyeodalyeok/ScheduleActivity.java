e03d0d699d1772db691e614980192b05c702dafb
unknown <soominnm@naver.com>
2019-11-21 00:13:12 +0900

    date update

--- DalyeoDalyeok/app/src/main/java/com/example/dalyeodalyeok/ScheduleActivity.java
+++ DalyeoDalyeok/app/src/main/java/com/example/dalyeodalyeok/ScheduleActivity.java
@@ -2,9 +2,11 @@ package com.example.dalyeodalyeok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,18 +16,24 @@ import com.example.dalyeodalyeok.ui.home.HomeFragment;
 
 public class ScheduleActivity extends AppCompatActivity {
 
-    int year;
-    int month;
-    int day;
+    //int year;
+    //int month;
+    //int day;
 
     String strY;
     String strM;
     String strD;
 
+    private TextView textView_Date;
+    private DatePickerDialog.OnDateSetListener callbackMethod;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        this.InitializeView();
+        this.InitializeListener();
 
 //        year = getIntent().getIntExtra("year", 2000);
 //        month = getIntent().getIntExtra("month", 8);
@@ -54,7 +62,29 @@ public class ScheduleActivity extends AppCompatActivity {
             }
         });
     }
+    public void InitializeView()
+    {
+        textView_Date = (TextView)findViewById(R.id.textView_date);
+    }
 
+    public void InitializeListener()
+    {
+        callbackMethod = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+            {
+                textView_Date.setText(year + "년" + monthOfYear + "월" + dayOfMonth + "일");
+            }
+        };
+    }
+
+    public void OnClickHandler(View view)
+    {
+        DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, 2019, 5, 24);
+
+        dialog.show();
+    }
 
     public void btn_Click(View view)
     {
